a816513749a0c4578a28687b01a4287b855863bc
Udeh Tochukwu Tobias <jaxj9449@gmail.com>
2025-05-05 13:25:00 +0100

    adjusted the backend logic

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-// Import required modules
+require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
-const dotenv = require('dotenv'); // To load environment variables
 const connectToDB = require('./database/db'); // Import the MongoDB connection function
 const invoiceRoutes = require('./routes/invoice-routes'); // Import your routes (adjust path as needed)
 const emailRoutes = require('./routes/email-routes');
 const cors = require('cors');
 
-// Load environment variables from the .env file
-dotenv.config();
 
 // Create an instance of the Express app
 const app = express();
 
 // Set the port from environment variable or fallback to 4000
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(cors());
